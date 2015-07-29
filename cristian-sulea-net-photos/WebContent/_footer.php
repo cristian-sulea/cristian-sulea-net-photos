2f7c1ce946403e3d21c82dd3d66922577a88a80b
Cristian Sulea <cristian@sulea.net>
2015-07-29 15:57:12 +0300

    no need for action links in footer - the copyright is enough

--- cristian-sulea-net-photos/WebContent/_footer.php
+++ cristian-sulea-net-photos/WebContent/_footer.php
@@ -1,11 +1,5 @@
 	<footer id="footer">
 
-		<ul class="actions">
-			<li><a href="https://www.linkedin.com/profile/view?id=173404791" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
-			<li><a href="https://plus.google.com/+CristianSulea79/about" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
-			<li><a href="https://github.com/cristian-sulea" class="icon fa-github"><span class="label">GitHub</span></a></li>
-		</ul>
-
 		<ul class="menu">
 			<li>&copy; <a href="http://cristian.sulea.net">Cristian Sulea</a></li>
 			<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>

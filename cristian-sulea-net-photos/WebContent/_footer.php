24589f44f674e97317ac0ab9d01e1b2a704457ed
Cristian Sulea <cristian@sulea.net>
2015-07-29 15:55:29 +0300

    fixed footer/actions with correct links

--- cristian-sulea-net-photos/WebContent/_footer.php
+++ cristian-sulea-net-photos/WebContent/_footer.php
@@ -1,10 +1,9 @@
 	<footer id="footer">
 
 		<ul class="actions">
-			<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
-			<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
-			<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
-			<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+			<li><a href="https://www.linkedin.com/profile/view?id=173404791" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+			<li><a href="https://plus.google.com/+CristianSulea79/about" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+			<li><a href="https://github.com/cristian-sulea" class="icon fa-github"><span class="label">GitHub</span></a></li>
 		</ul>
 
 		<ul class="menu">
